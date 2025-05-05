a82915ec9852d5e8cc3cdf7b22e1f2093970e3b3
Anwar Hossain <mahedianwar@gmail.com>
2025-05-05 20:54:36 +0600

    feat: update environment variables for Clerk integration and enhance middleware for route protection

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,8 +1,9 @@
-// middleware.ts
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  publicRoutes: ['/', '/sign-in(.*)', '/sign-up(.*)', '/api/webhooks(.*)'],
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
